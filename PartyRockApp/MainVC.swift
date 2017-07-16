e7e9b0079fa7df07fe1bc14252f02501f2e69ff5
Sattar Hummatli <settarxan@gmail.com>
2017-07-16 12:16:42 +0400

    Created project and desgned views

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Settar Hummetli on 7/16/17.
+//  Copyright © 2017 Settar Hummetli. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
+
